e15960d3d3fb37257d00795f1e831a5bd429fcef
azu <azuciao@gmail.com>
2016-04-30 10:43:38 +0900

    test: add destructuring test

---
+++ test/fixtures/ExportDefaultFunctionDestructuring/actual.js
@@ -0,0 +1,6 @@
+/**
+ * @param {string} key
+ */
+export default function fn({key}) {
+
+}
---
+++ test/fixtures/ExportDefaultFunctionDestructuring/expected.js
@@ -0,0 +1,6 @@
+/**
+ * @param {string} key
+ */
+export default function fn({ key }) {
+  console.assert(typeof key === "string");
+}
