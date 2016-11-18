0a506de9e1737eeed14968b5d14678e4d767ae10
azu <azuciao@gmail.com>
2016-11-19 00:08:04 +0900

    fix(test): update deps

--- test/fixtures/TypeCustomObject/expected.js
+++ test/fixtures/TypeCustomObject/expected.js
@@ -2,4 +2,4 @@
  * @type {MyObject}
  */
 const value = {};
-console.assert(typeof MyObject === "undefined" || typeof MyObject !== "function" || value instanceof MyObject);
+console.assert(typeof Symbol === "function" && typeof Symbol.hasInstance === "symbol" && typeof MyObject !== "undefined" && typeof MyObject[Symbol.hasInstance] === "function" ? MyObject[Symbol.hasInstance](value) : typeof MyObject === "undefined" || typeof MyObject !== "function" || value instanceof MyObject);
