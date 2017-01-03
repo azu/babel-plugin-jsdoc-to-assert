2ec1c3b2b2013701dddb37c94b76db853731c77c
azu <azuciao@gmail.com>
2017-01-03 13:48:10 +0900

    test: add tests for Destructuring issue
    https://github.com/azu/jsdoc-to-assert/issues/15

---
+++ test/fixtures/Destructuring_with_es2015/actual.js
@@ -0,0 +1,8 @@
+class Foo {
+  /**
+   * @param {string} x
+   */
+  constructor({x}) {
+    this.x = x
+  }
+}
---
+++ test/fixtures/Destructuring_with_es2015/expected.js
@@ -0,0 +1,13 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Foo =
+/**
+ * @param {string} x
+ */
+function Foo(_ref) {
+  var x = _ref.x;
+
+  _classCallCheck(this, Foo);
+
+  this.x = x;
+};
