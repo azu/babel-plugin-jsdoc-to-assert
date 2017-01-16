29b8879fd4f547e5152da5a3757ec42843bc858c
Victor Homyakov <vkhomyackov@gmail.com>
2017-01-16 13:51:53 +0300

    fix(test): normalize line endings for different platforms/git settings; make spec output more readable

--- test/babel-plugin-jsdoc-to-assert-test.js
+++ test/babel-plugin-jsdoc-to-assert-test.js
@@ -1,16 +1,17 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-import plugin from '../src/';
+import '../src/';
 import {transformFileSync} from 'babel-core';
-function trim(str) {
-    return str.replace(/^\s+|\s+$/, '');
+
+function normalize(str) {
+    return str.replace(/^\s+|\s+$/, '').replace(/\r?\n/g, '\n');
 }
 
-describe('finds JSDoc', () => {
+describe('JSDoc plugin', () => {
     const fixturesDir = path.join(__dirname, 'fixtures');
     fs.readdirSync(fixturesDir).map((caseName) => {
-        it(`should ${caseName.split('-').join(' ')}`, () => {
+        it(`should generate assertions for ${caseName.replace(/-/g, ' ')}`, () => {
             const fixtureDir = path.join(fixturesDir, caseName);
             let actualPath = path.join(fixtureDir, 'actual.js');
             const actual = transformFileSync(actualPath).code;
@@ -24,7 +25,7 @@ describe('finds JSDoc', () => {
                 path.join(fixtureDir, 'expected.js')
             ).toString().replace(/%FIXTURE_PATH%/g, actualPath);
 
-            assert.equal(trim(actual), trim(expected));
+            assert.equal(normalize(actual), normalize(expected));
         });
     });
 });
