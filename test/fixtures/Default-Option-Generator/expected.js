52762f112683b94a8173cb0f4f9db96b1c2ed0a1
Victor Homyakov <vkhomyackov@gmail.com>
2017-01-16 13:54:15 +0300

    fix(generator): simplify generated assertion

--- test/fixtures/Default-Option-Generator/expected.js
+++ test/fixtures/Default-Option-Generator/expected.js
@@ -3,15 +3,8 @@
  * @param {string[]} y - this is a param.
  */
 function myFunc(x, y) {
-  if (!(typeof x === "number")) {
-    console.assert(typeof x === "number", 'Expected type: @param {number} x\nActual value:', x, '\nFailure assertion: typeof x === "number"');
-  }
-
-  if (!(Array.isArray(y) && y.every(function (item) {
+  console.assert(typeof x === "number", 'Expected type: @param {number} x\nActual value:', x, '\nFailure assertion: typeof x === "number"');
+  console.assert(Array.isArray(y) && y.every(function (item) {
     return typeof item === "string";
-  }))) {
-    console.assert(Array.isArray(y) && y.every(function (item) {
-      return typeof item === "string";
-    }), 'Expected type: @param {Array.<string>} y\nActual value:', y, '\nFailure assertion: Array.isArray(y) && y.every(function(item){ return (typeof item === "string"); })');
-  }
+  }), 'Expected type: @param {Array.<string>} y\nActual value:', y, '\nFailure assertion: Array.isArray(y) && y.every(function(item){ return (typeof item === "string"); })');
 }
--- test/fixtures/SpecGenerator-type/expected.js
+++ test/fixtures/SpecGenerator-type/expected.js
@@ -5,17 +5,8 @@ const value = "s";
 /**
  * @type {string[]}
  */
-
-if (!(typeof value === "string")) {
-  console.assert(typeof value === "string", 'Expected type: @type {string} value\nActual value:', value, '\nFailure assertion: typeof value === "string"');
-}
-
+console.assert(typeof value === "string", 'Expected type: @type {string} value\nActual value:', value, '\nFailure assertion: typeof value === "string"');
 const array = ["s"];
-
-if (!(Array.isArray(array) && array.every(function (item) {
+console.assert(Array.isArray(array) && array.every(function (item) {
   return typeof item === "string";
-}))) {
-  console.assert(Array.isArray(array) && array.every(function (item) {
-    return typeof item === "string";
-  }), 'Expected type: @type {Array.<string>} array\nActual value:', array, '\nFailure assertion: Array.isArray(array) && array.every(function(item){ return (typeof item === "string"); })');
-}
+}), 'Expected type: @type {Array.<string>} array\nActual value:', array, '\nFailure assertion: Array.isArray(array) && array.every(function(item){ return (typeof item === "string"); })');
