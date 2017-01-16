2fbd71e9f9885f17a115d86e4d50caf84e73745f
azu <azuciao@gmail.com>
2017-01-16 20:55:56 +0900

    chore: remove unused import

--- test/babel-plugin-jsdoc-to-assert-test.js
+++ test/babel-plugin-jsdoc-to-assert-test.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
-import '../src/';
 import {transformFileSync} from 'babel-core';
 
 function normalize(str) {
